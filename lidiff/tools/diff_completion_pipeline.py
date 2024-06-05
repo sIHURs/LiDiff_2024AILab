0a260ab597a1e2d5b5bc7f6fb681005cb45e482c
yifancodeWin <yifan.wang2@stud.uni-hannover.de>
2024-06-06 00:23:31 +0200

    pipeline, model, point cloud processing

--- lidiff/tools/diff_completion_pipeline.py
+++ lidiff/tools/diff_completion_pipeline.py
@@ -12,17 +12,22 @@ from natsort import natsorted
 import click
 import time
 
+# 设定 model
 class DiffCompletion(LightningModule):
     def __init__(self, diff_path, refine_path, denoising_steps, cond_weight):
         super().__init__()
+        
+        # ! 读取预存的模型超参数
         ckpt_diff = torch.load(diff_path)
         self.save_hyperparameters(ckpt_diff['hyper_parameters'])
         assert denoising_steps <= self.hparams['diff']['t_steps'], \
         f"The number of denoising steps cannot be bigger than T={self.hparams['diff']['t_steps']} (you've set '-T {denoising_steps}')"
 
+        # ! 创建模型 
         self.partial_enc = minknet.MinkGlobalEnc(in_channels=3, out_channels=self.hparams['model']['out_dim']).cuda()
         self.model = minknet.MinkUNetDiff(in_channels=3, out_channels=self.hparams['model']['out_dim']).cuda()
         self.model_refine = minknet.MinkUNet(in_channels=3, out_channels=3*6)
+        # ! 载入训练好的参数
         self.load_state_dict(ckpt_diff['state_dict'], strict=False)
 
         ckpt_refine = torch.load(refine_path)
@@ -33,7 +38,7 @@ class DiffCompletion(LightningModule):
         self.model_refine.eval()
         self.cuda()
 
-        # for fast sampling
+        # ! for fast sampling
         self.hparams['diff']['s_steps'] = denoising_steps
         self.dpm_scheduler = DPMSolverMultistepScheduler(
                 num_train_timesteps=self.hparams['diff']['t_steps'],
@@ -66,11 +71,14 @@ class DiffCompletion(LightningModule):
         self.dpm_scheduler.sigmas = self.dpm_scheduler.sigmas.cuda()
 
     def points_to_tensor(self, points):
+        # ! 将点云原始数据转换为Minkowski Engine可以处理的批处理坐标
         x_feats = ME.utils.batched_coordinates(list(points[:]), dtype=torch.float32, device=self.device)
 
         x_coord = x_feats.clone()
+        # ! 将克隆的数据按分辨率进行量化
         x_coord = torch.round(x_coord / self.hparams['data']['resolution'])
 
+        # ! 将一个点云数据集转换为Minkowski Engine中ME.TensorField的对象
         x_t = ME.TensorField(
             features=x_feats[:,1:],
             coordinates=x_coord,
@@ -83,6 +91,7 @@ class DiffCompletion(LightningModule):
 
         return x_t                                                                                        
 
+    # ? 这里为什么需要输入 x_uncond， 既然x_uncond是一个空的tensor
     def reset_partial_pcd(self, x_part, x_uncond):
         x_part = self.points_to_tensor(x_part.F.reshape(1,-1,3).detach())
         x_uncond = self.points_to_tensor(torch.zeros_like(x_part.F.reshape(1,-1,3)))
@@ -90,23 +99,31 @@ class DiffCompletion(LightningModule):
         return x_part, x_uncond
 
     def preprocess_scan(self, scan):
+        # ! 重点： 删除了threshold外的所有点，对于从shape到scene有什么影响
+        # ! 计算每个点到原点的欧几里得距离
         dist = np.sqrt(np.sum((scan)**2, -1))
+        # ! 过滤点云， 保留距离在指定范围内的点
         scan = scan[(dist < self.hparams['data']['max_range']) & (dist > 3.5)][:,:3]
 
         # use farthest point sampling
+        # * Farthest Point Sampling (FPS) 是一种常用于点云数据处理的采样算法，
+        # * 旨在从一个点集合中选择具有最大距离的子集点。FPS 在点云降采样、3D 形状分析和计算机图形学等领域非常有用 - chatGPT
         pcd_scan = o3d.geometry.PointCloud()
         pcd_scan.points = o3d.utility.Vector3dVector(scan)
         pcd_scan = pcd_scan.farthest_point_down_sample(int(self.hparams['data']['num_points'] / 10))
         scan = torch.tensor(np.array(pcd_scan.points)).cuda()
         
+        # ! 重复采样后的点云数据10次
         scan = scan.repeat(10,1)
-        scan = scan[None,:,:]
+        scan = scan[None,:,:] # 增加一个维度
 
         return scan
 
     def postprocess_scan(self, completed_scan, input_scan):
         dist = np.sqrt(np.sum((completed_scan)**2, -1))
         post_scan = completed_scan[dist < self.hparams['data']['max_range']]
+        # ! 获取输入扫描数据的最大 z 值。
+        # ! 计算输入扫描数据 z 坐标的均值减去两倍标准差的值，作为最小 z 值。
         max_z = input_scan[...,2].max().item()
         min_z = (input_scan[...,2].mean() - 2 * input_scan[...,2].std()).item()
 
@@ -115,8 +132,11 @@ class DiffCompletion(LightningModule):
         return post_scan
 
     def complete_scan(self, scan):
-        scan = self.preprocess_scan(scan)
-        x_feats = scan + torch.randn(scan.shape, device=self.device)
+        # ! 在这里还是有少许问题，关于classifier free guidance的作用与原理还是有些不太明白，具体他是如何参与计算的与对输出的影响是什么
+        # ! 与下面 x_feats加入了噪音
+        
+        scan = self.preprocess_scan(scan) # ! 下降采样， 降低密度
+        x_feats = scan + torch.randn(scan.shape, device=self.device) # ? 加入了随机的 噪音 对比于stable diffusion 也有这一个操作， 但是与diffusion的添加noise不是同一个
         x_full = self.points_to_tensor(x_feats)
         x_cond = self.points_to_tensor(scan)
         x_uncond = self.points_to_tensor(torch.zeros_like(scan))
@@ -184,10 +204,12 @@ def load_pcd(pcd_file):
 def main(diff, refine, denoising_steps, cond_weight):
     exp_dir = diff.split('/')[-1].split('.')[0].replace('=','') + f'_T{denoising_steps}_s{cond_weight}'
 
+    # ! 相当于建立pipeline
     diff_completion = DiffCompletion(
             diff, refine, denoising_steps, cond_weight
         )
 
+    # ! 准备数据集
     path = './Datasets/test/'
 
     os.makedirs(f'./results/{exp_dir}/refine', exist_ok=True)
@@ -195,12 +217,15 @@ def main(diff, refine, denoising_steps, cond_weight):
 
     for pcd_path in tqdm.tqdm(natsorted(os.listdir(path))):
         pcd_file = os.path.join(path, pcd_path)
+        # ! 加载点云 数据
         points = load_pcd(pcd_file)
     
         start = time.time()
         refine_scan, diff_scan = diff_completion.complete_scan(points)
         end = time.time()
         print(f'took: {end - start}s')
+        
+        # ! 可视化
         pcd_refine = o3d.geometry.PointCloud()
         pcd_refine.points = o3d.utility.Vector3dVector(refine_scan)
         pcd_refine.estimate_normals()
